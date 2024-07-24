fb5133cb3ff0f71c37faf4831d2ab1e2aa6e3882
Adam Marsal <adam.marsal@justice.gouv.fr>
2024-07-24 10:59:54 +0200

    update l'installation : ajout crayon pour message en bleu

--- R/install_packages_dbexplorer.R
+++ R/install_packages_dbexplorer.R
@@ -1,4 +1,5 @@
-install.packages("shinyAce")
+lib_to_install="~/R_Commun/Adam/custom_lib/db-explorer/"
+# lib_to_install=.libPaths()[1]
 
 run_vanilla <- function(cmd){
   
@@ -6,13 +7,16 @@ run_vanilla <- function(cmd){
   system(system_cmd)
 }
 
-install.packages("fontawesome",lib="~/R_Commun/Adam/custom_lib/db-explorer/")
-install.packages("shinyAce",lib="~/R_Commun/Adam/custom_lib/db-explorer/")
-run_vanilla('remotes::install_version("DBI","1.1-3",upgrade = "never",lib="~/R_Commun/Adam/custom_lib/db-explorer/",repos="https://cloud.r-project.org/")')
-run_vanilla('remotes::install_version("pillar","1.9.0",upgrade = "never",lib="~/R_Commun/Adam/custom_lib/db-explorer/",repos="https://cloud.r-project.org/")')
-run_vanilla('remotes::install_version("tibble","3.2.1",upgrade = "never",lib="~/R_Commun/Adam/custom_lib/db-explorer/",repos="https://cloud.r-project.org/")')
-run_vanilla('remotes::install_version("tidyselect","1.2.0",upgrade = "never",lib="~/R_Commun/Adam/custom_lib/db-explorer/",repos="https://cloud.r-project.org/")')
-run_vanilla('remotes::install_version("dplyr","1.1.2",upgrade = "never",lib="~/R_Commun/Adam/custom_lib/db-explorer/",repos="https://cloud.r-project.org/")')
-run_vanilla('remotes::install_version("glue","1.6.2",upgrade = "never",lib="~/R_Commun/Adam/custom_lib/db-explorer/",repos="https://cloud.r-project.org/")')
-run_vanilla('remotes::install_version("dbplyr","2.4-0",upgrade = "never",lib="~/R_Commun/Adam/custom_lib/db-explorer/",repos="https://cloud.r-project.org/")')
+install.packages("fontawesome",lib=lib_to_install)
+install.packages("shinyAce",lib=lib_to_install)
+install.packages("shinyhelper",lib=lib_to_install)
+
+run_vanilla(glue::glue('install.packages("crayon",lib="{lib_to_install}",repos="https://cloud.r-project.org/")'))
+run_vanilla(glue::glue('remotes::install_version("DBI","1.1-3",upgrade = "never",lib="{lib_to_install}",repos="https://cloud.r-project.org/")'))
+run_vanilla(glue::glue('remotes::install_version("pillar","1.9.0",upgrade = "never",lib="{lib_to_install}",repos="https://cloud.r-project.org/")'))
+run_vanilla(glue::glue('remotes::install_version("tibble","3.2.1",upgrade = "never",lib="{lib_to_install}",repos="https://cloud.r-project.org/")'))
+run_vanilla(glue::glue('remotes::install_version("tidyselect","1.2.0",upgrade = "never",lib="{lib_to_install}",repos="https://cloud.r-project.org/")'))
+run_vanilla(glue::glue('remotes::install_version("dplyr","1.1.2",upgrade = "never",lib="{lib_to_install}",repos="https://cloud.r-project.org/")'))
+run_vanilla(glue::glue('remotes::install_version("glue","1.6.2",upgrade = "never",lib="{lib_to_install}",repos="https://cloud.r-project.org/")'))
+run_vanilla(glue::glue('remotes::install_version("dbplyr","2.4-0",upgrade = "never",lib="{lib_to_install}",repos="https://cloud.r-project.org/")'))
 
