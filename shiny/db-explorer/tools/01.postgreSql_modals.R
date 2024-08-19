7c2a81fefdf264cedfc9ebcf10884e670158f51b
Adam Marsal <adam.marsal@justice.gouv.fr>
2024-08-19 14:22:22 +0200

    nettoyage

--- shiny/db-explorer/tools/01.postgreSql_modals.R
+++ shiny/db-explorer/tools/01.postgreSql_modals.R
@@ -1,18 +1,3 @@
-pg_connexion_modal <- function(failed = FALSE) {
-  modalDialog(
-    tags$h2('Login for PostgreSQL (Production)'),
-    textInput('username_pg', 'Username',value = system("whoami", intern = TRUE)),
-    passwordInput('password_pg', 'Password'),
-    if (failed)
-      div(tags$b("Login incorrect", style = "color: red;")),
-    footer=tagList(
-      actionButton('submit_pg_login', 'Submit'),
-      modalButton('cancel')
-    )
-  )
-}
-
-
 connexion_modal <- function(failed = FALSE,title='Login for PostgreSQL (Production)') {
   modalDialog(
     tags$h2(title),
