7a18895e376af2315c4981f2f48e5e3a384fc6b7
Adam Marsal <adam.marsal@justice.gouv.fr>
2024-08-27 08:39:36 +0200

    clean script push

--- R/push_app_toRcommun.R
+++ R/push_app_toRcommun.R
@@ -1,4 +1,4 @@
-out="~/R_Commun/Adam/db-explorer/"
+out="~/R_Commun/Adam/db-explorer_test/"
 unlink(out,recursive = T)
 dir.create(out)
 file.copy(from = list.files("./shiny/db-explorer/", full.names = TRUE), 
@@ -6,14 +6,5 @@ file.copy(from = list.files("./shiny/db-explorer/", full.names = TRUE),
           recursive = TRUE)
 
 
-# options(dbexplorer_dbhost="Netezza")
-# options(dbexplorer_dbhost="")
-
-# dbexplorer_dbhost="Netezza"
-# dbexplorer_schema="DTL_ADAM_MARSAL"
-# dbexplorer_table = "DP1602_OBLIGATIONS"
 launch_shiny_backgroundJob(out,name = "db-explorer")
 
-# dbexplorer_dbhost=""
-# dbexplorer_schema=""
-# launch_shiny_backgroundJob(out,name = "db-explorer")
