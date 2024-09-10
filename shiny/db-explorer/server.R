5cfd7ccab224fe936af4e803e7a3d8d3fc01532e
MARSAL Adam <adam.marsal@justice.gouv.fr>
2024-09-10 13:51:31 +0200

    Master

--- shiny/db-explorer/server.R
+++ shiny/db-explorer/server.R
@@ -19,7 +19,7 @@ shinyServer(function(input, output, session) {
   })
   
   current_time<-reactiveTimer()
-  
+
   observeEvent(input$changed_value,{
     logg_full <<- c(logg_full,input$changed_value)
   })
@@ -30,12 +30,6 @@ shinyServer(function(input, output, session) {
       stopApp()
     }
   })
-
-  rv <- reactiveValues(
-    tab_created = 1,
-    tab_set_panel=c("Table_1"),
-    logg_full = c()
-  )
   
   logins <- reactiveValues()
   
@@ -43,37 +37,8 @@ shinyServer(function(input, output, session) {
   
   viewSqlServer("Onglet_1",parent_session=session,logins=logins)
   
-  ### Input trigger depuis le script js CreateTabButton.
-  observeEvent(input$create_tab,{
-    
-    rv$tab_created <- rv$tab_created+1
-    tab_id         <- paste0("Onglet_",rv$tab_created)
-    tab_title      <- paste0("Table_",rv$tab_created)
-    viewTabServer(tab_id,parent_session=session,logins=logins)
-    
-    insertTab("TABSETPANEL",
-              target=tail(rv$tab_set_panel,n=1),
-              position = 'after',
-              tab=tabPanel(value=tab_title,
-                           title = tab_title_removable(tab_title),
-                           viewTabUi(tab_id)),
-              select=TRUE
-    )
-    rv$tab_set_panel <- c(rv$tab_set_panel,tab_title)
-
-  })
-  
-  
-  observeEvent(input$remove_tab, {
-    
-    if(length(rv$tab_set_panel)>1){
-      active_tab=input$TABSETPANEL
-      removeTab(inputId = "TABSETPANEL", target = input$remove_tab)
-      updateTabsetPanel(inputId = "TABSETPANEL",selected = active_tab)
-      rv$tab_set_panel <- rv$tab_set_panel[!rv$tab_set_panel == input$remove_tab]
-    }
-    
-  })
-  
+  rv <- reactiveValues(
+    logg_full = c()
+  )
 
 })
--- shiny/db-explorer/www/js/CreateTabButton.js
+++ shiny/db-explorer/www/js/CreateTabButton.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-      var button = "<li class='list_button'><button type='button' class='btn btn-success' onclick='Shiny.onInputChange(\"create_tab\", Math.random())'><i class='fa fa-plus'></i></button></li>";
-                $("#TABSETPANEL").first().append(button);
-  });
-  
+    var navig_add_button = "<li class='list_button'>" +
+                 "<button type='button' class='btn btn-success' " +
+                 "onclick='Shiny.onInputChange(\"navig_create_tab\", Math.random())'>" +
+                 "<i class='fa fa-plus'></i>" +
+                 "</button>" +
+                 "</li>";
+    $("#Navig_tabset_panel").first().append(navig_add_button);
+    
+    var sql_add_button = "<li class='list_button'>" +
+                 "<button type='button' class='btn btn-success' " +
+                 "onclick='Shiny.onInputChange(\"sql_create_tab\", Math.random())'>" +
+                 "<i class='fa fa-plus'></i>" +
+                 "</button>" +
+                 "</li>";
+    $("#ConsoleSQL_tabset_panel").first().append(sql_add_button);
+    
+});
