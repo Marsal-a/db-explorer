d9bbf03900a34a371ea0a72fbf6c9f48ecb0660d
Adam Marsal <adam.marsal@justice.gouv.fr>
2024-10-30 16:49:43 +0100

    Suppression des fichiers maintenant ignorés

--- dev/test_divers/RunAppJob/test_runJob_shiny.R
+++
@@ -1,8 +0,0 @@
-myGUI <- function() {
-  print(getwd())
-  appDir <- "explorations/FiF/"
-  appDir <- "../../explorations/FiF/"
-  shiny::runApp(appDir = appDir, display.mode = "normal", quiet = TRUE, host = "127.0.0.1", port = 3838)
-}
-myGUI()
-
