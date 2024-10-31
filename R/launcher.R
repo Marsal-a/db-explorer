2b509d82013a5c61f2b9d59343cfc7a0ac9312e1
Adam Marsal <adam.marsal@justice.gouv.fr>
2024-10-31 15:38:57 +0100

    update documentation du lanceur

--- R/launcher.R
+++ R/launcher.R
@@ -1,8 +1,8 @@
-#' Lancement d'une appli Shiny en fond
+#' Lancement d'une appli Shiny en background
 #'
 #' @param shinyPath le chemin de l'application shiny
+#' @param host l'adresse de connexion à l'application
 #' @param port le port d'ecoute
-#' @param sleep temp sleep avant lancement
 #' @param name nom du job
 #' @param opt_cmd commande optionnelle à lancer avant le run de l'appl
 #'
@@ -11,21 +11,14 @@
 #' @importFrom httr GET use_proxy
 #' @importFrom utils browseURL
 #' @return rien
-launch_shiny_backgroundJob <- function(shinyPath,port=NULL,sleep=5,name=NULL,opt_cmd=NULL){
+launch_shiny_backgroundJob <- function(shinyPath,host="127.0.0.1",port=NULL,name=NULL,opt_cmd=NULL,max_attempts=30){
 
-  if(is.null(port)){
-    for (i in 1:20) {
-      while (TRUE) {
-        port <- shiny:::p_randomInt(3000, 8000)
-        if (!port %in% c(3659, 4045, 5060, 5061, 6000, 6566, 6665:6669, 6697)) {
-          break
-        }
-      }
-    }
+  if (is.null(port)) {
+    port <- httpuv::randomPort(min=3000,max=15000,n=50)
   }
 
-
-  ### La fonction jobRunScript ne peut utiliser qu'un script, il faut donc ecrire cette commande dans un fichier :
+  ### La fonction jobRunScript ne peut prendre qu'un script en entrée (et pas une commande),
+  ### il faut donc ecrire cette commande dans un fichier :
   script=""
   script=c(script,opt_cmd)
   script=c(script,glue::glue("shiny::runApp(appDir = \"{shinyPath}\",
@@ -34,17 +27,18 @@ launch_shiny_backgroundJob <- function(shinyPath,port=NULL,sleep=5,name=NULL,opt
                     host = \"127.0.0.1\",
                     port = {port})"))
 
-  filename=tempfile("db-explorer")
+  filename <- tempfile("db-explorer")
   fileConn <- file(filename)
   writeLines(c(script),fileConn)
   close.connection(fileConn)
 
+  print(filename)
 
   job <- rstudioapi::jobRunScript(path = filename ,importEnv =FALSE,exportEnv = "R_GlobalEnv",name = paste0(name," (port = ", port,")"))
 
   url <- glue::glue("http://127.0.0.1:{port}")
 
-  test_url <- function(url) {
+  isUrlReady <- function(url) {
     response <- tryCatch({
       httr::GET(url,httr::use_proxy(""))
     }, error = function(e) {
@@ -59,11 +53,16 @@ launch_shiny_backgroundJob <- function(shinyPath,port=NULL,sleep=5,name=NULL,opt
   }
 
 
-  max_attempts <- 30
+  ## L'application shiny peut prendre quelques secondes pour être "up"
+  ## Le lancement de l'app via shiny::runApp démarre un serveur et le chargement
+  ## des packages nécessaires peut prendre du temps
+  ## Pour ouvrir l'application, on procède à {max_attemps} tentatives de connexion
+  ## et on attends 1 secondes entre chaque tentative.
+  ## Si l'url répond, on ouvre l'application et on repositionne rstudio sur la
+  ## console (car par défault se positionne sur l'onglet job)
 
   for (i in 1:max_attempts) {
-
-    if (test_url(url)) {
+    if (isUrlReady(url)) {
       utils::browseURL(url)
       rstudioapi::executeCommand("activateConsole")
       break
@@ -72,6 +71,7 @@ launch_shiny_backgroundJob <- function(shinyPath,port=NULL,sleep=5,name=NULL,opt
     }
   }
 
+  unlink(filename)
   return(invisible(NULL))
 
 }
