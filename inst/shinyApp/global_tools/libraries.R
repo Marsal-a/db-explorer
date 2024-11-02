3843ce11c2563df5cc79b7e8574c74b0c8416e52
Adam Marsal <adam.marsal@justice.gouv.fr>
2024-11-02 22:42:50 +0100

    simplifcation des librairries avec une otpion

--- inst/shinyApp/global_tools/libraries.R
+++ inst/shinyApp/global_tools/libraries.R
@@ -1,13 +1,3 @@
-## Si l'application est lancé depuis R_Commun ou depuis le projet R, on source des versions particulières des librairies.
-if(grepl("db-explorer/inst/shinyApp|R_Commun",getwd())){
-  .libPaths(c( "~/R_Commun/Adam/custom_lib/db-explorer/",.libPaths()))
-}
-
-## Si l'application est lancé via la fonction du package, on source les librairies depuis le répertoire du package
-if(grepl("/4.1/dbExplorer/shinyApp",getwd())){
-  .libPaths(c(system.file("library",package = "dbExplorer"),.libPaths()))
-}
-
 library(magrittr)
 library(crayon)
 library(shinyAce)
